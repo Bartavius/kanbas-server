e3d1d77ce6ececa2b94257c2065a0217fc20f911
Jirath Lojanarungsiri <lojanarungsiri.j@northeastern.edu>
2024-11-19 19:52:54 -0500

    3.5 completed

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import Hello from "./Hello.js";
 import Lab5 from "./Lab5/index.js";
+import cors from "cors";
 const app = express();
-//const app = require('express');
+app.use(cors());
 Lab5(app);
 Hello(app);
 app.listen(process.env.PORT || 4000);
