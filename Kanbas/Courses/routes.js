36e8019ef43d3dc16ed0421c7968c7379cdb9aeb
Jirath Lojanarungsiri <lojanarungsiri.j@northeastern.edu>
2024-12-06 20:31:16 -0500

    fixed rendering issue with users and implemented fetchCourse/createCourse

--- Kanbas/Courses/routes.js
+++ Kanbas/Courses/routes.js
@@ -31,8 +31,8 @@ export default function CourseRoutes(app) {
       }
     res.send(status);
   });
-  app.get("/api/courses", (req, res) => {
-    const courses = dao.findAllCourses();
+  app.get("/api/courses", async (req, res) => {
+    const courses = await dao.findAllCourses();
     res.send(courses);
   });
 
---
+++ Kanbas/Courses/schema.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+// start/end date are strings for now but will eventually be stored as Date
+const courseSchema = new mongoose.Schema(
+ {
+   name: String,
+   number: String,
+   startDate: String,
+   endDate: String,
+   department: String,
+   credits: Number,
+   description: String,
+   image: {
+    type: String,
+    default: "/images/reactjs.jpg"
+   },
+   author: String,
+ },
+ { collection: "courses" }
+);
+export default courseSchema;
