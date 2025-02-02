e59700a4d37bf7a9eac0be0c34050e8da1284bf0
Bartavius <lojanarungsiri.j@northeastern.edu>
2025-02-01 20:09:07 -0500

    changes..

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import express from 'express';
 import Hello from "./Hello.js";
-import Lab5 from "./Lab5/index.js";
 import cors from "cors";
 import session from "express-session";
 import "dotenv/config";
